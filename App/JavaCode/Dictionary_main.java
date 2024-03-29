0fcef3dc19b66b4760b27e7877f05127d8ee7617
Quang Dũng <105673472+quangdung20@users.noreply.github.com>
2024-03-29 17:23:18 +0700

    set layer main fit size, change coler alls button

--- App/JavaCode/Dictionary_main.java
+++ App/JavaCode/Dictionary_main.java
@@ -4,22 +4,32 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class Dictionary_main extends Application {
-
     private static Stage stg;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         stg = primaryStage;
-        primaryStage.setResizable(false);
         Parent root = FXMLLoader.load(getClass().getResource("Layers/LoginLayer.fxml"));
-        primaryStage.setTitle("Falcon");
-        primaryStage.setScene(new Scene(root, 700, 550));
+        primaryStage.setTitle("Dictionary Application");
+        primaryStage.setResizable(false);
+        ClassLoader classLoader = Dictionary_main.class.getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream("resources/Image/logo.png");
+        assert inputStream != null;
+        Image image = new Image(inputStream);
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.getIcons().add(image);
         primaryStage.show();
+
     }
 
     public void changeScene(String fxml) throws IOException {
