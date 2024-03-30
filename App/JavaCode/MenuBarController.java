62d1dd4b59a351140597536cf089fb7378fb7519
Quang Dũng <105673472+quangdung20@users.noreply.github.com>
2024-03-30 16:45:07 +0700

    create file menubarlayer and menubarcontrol

---
+++ App/JavaCode/MenuBarController.java
@@ -0,0 +1,24 @@
+package JavaCode;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+public class MenuBarController {
+    @FXML
+    private Button addWordBtn;
+
+    @FXML
+    private Button btnLogout;
+
+    @FXML
+    private AnchorPane container;
+
+    @FXML
+    private Button learningEngBtn;
+
+    @FXML
+    private Button searchBtn;
+
+    @FXML
+    private Button translateBtn;
+}
